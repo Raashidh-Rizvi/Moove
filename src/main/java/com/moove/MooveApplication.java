5a8e3a397daf2fffcc840d5fe57f27a6d2f597a6
Raashidh <raashidh24@gmail.com>
2025-07-05 20:45:40 +0530

    Initial commit

---
+++ src/main/java/com/moove/MooveApplication.java
@@ -0,0 +1,13 @@
+package org.example.moove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MooveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moove/MooveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.moove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MooveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
